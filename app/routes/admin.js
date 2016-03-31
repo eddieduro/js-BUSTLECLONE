6d00dff747ea2a9576fe31d06fb246487a5e6c08
eddie <e.jamesduro@gmail.com>
2016-03-30 23:01:12 -0700

    clean up commented out code

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -5,28 +5,5 @@ export default Ember.Route.extend({
     return this.store.findAll('user');
   },
   actions: {
-  //   // isUserLogin(boolean) {
-  //   //   this.set('isUserLogin', boolean);
-  //   // },
-    userLogin(user, params, loggedIn){
-       var admin = Ember.Object.create({
-            loggedIn: false
-          })
-      Object.keys(params).forEach(function(key){
-          if(params[key]!==undefined) {
-            user.set(key, params[key]);
-            // console.log(key)
-          }
-        });
-        if(loggedIn[0] === "true"){
-          var admin = Ember.Object.create({
-            loggedIn: true
-          })
-          this.transitionTo('admin');
-        }
-      // console.log(admin);
-    },
-    // updateLogin(loggedIn){
-    // }
   }
 });
